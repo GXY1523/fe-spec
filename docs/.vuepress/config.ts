da8c6619df6446b19b3c657b36b6e06101d10e51
GXY <gxy252@outlook.com>
2024-04-11 21:50:53 +0800

    docs: 修改站点npm包

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,130 +1,147 @@
-import { defineConfig4CustomTheme, UserPlugins } from "vuepress/config";
+import { defineConfig4CustomTheme, UserPlugins } from 'vuepress/config';
 
 export default defineConfig4CustomTheme({
   locales: {
-    "/": {
-      lang: "zh-CN",
-      title: "XY",
-      description: "前端编码规范工程化",
+    '/': {
+      lang: 'zh-CN',
+      title: 'XY',
+      description: '前端编码规范工程化',
     },
   },
-  base: "/fe-spec/",
+  base: '/fe-spec/',
   themeConfig: {
     nav: [
-      { text: "首页", link: "/index.md" },
+      { text: '首页', link: '/index.md' },
       {
-        text: "编码规范",
+        text: '编码规范',
         items: [
-          { text: "HTML 编码规范", link: "/coding/html.md" },
-          { text: "CSS 编码规范", link: "/coding/css.md" },
-          { text: "JavaScript 编码规范", link: "/coding/javascript.md" },
-          { text: "Node 编码规范", link: "/coding/node.md" },
-          { text: "Typescript 编码规范", link: "/coding/typescript.md" },
+          { text: 'HTML 编码规范', link: '/coding/html.md' },
+          { text: 'CSS 编码规范', link: '/coding/css.md' },
+          { text: 'JavaScript 编码规范', link: '/coding/javascript.md' },
+          { text: 'Node 编码规范', link: '/coding/node.md' },
+          { text: 'Typescript 编码规范', link: '/coding/typescript.md' },
         ],
       },
       {
-        text: "工程规范",
+        text: '工程规范',
         items: [
-          { text: "Git 规范", link: "/engineering/git.md" },
-          { text: "文档规范", link: "/engineering/doc.md" },
-          { text: "CHANGELOG 规范", link: "/engineering/changelog.md" },
+          { text: 'Git 规范', link: '/engineering/git.md' },
+          { text: '文档规范', link: '/engineering/doc.md' },
+          { text: 'CHANGELOG 规范', link: '/engineering/changelog.md' },
+        ],
+      },
+      {
+        text: 'NPM包',
+        items: [
+          { text: 'xy-commitlint-config', link: '/npm/commitlint.md' },
+          { text: 'xy-stylelint-config', link: '/npm/stylelint.md' },
+          { text: 'xy-markdownlint-config', link: '/npm/markdownlint.md' },
+          { text: 'xy-eslint-config', link: '/npm/eslint.md' },
+          { text: 'xy-eslint-plugin', link: '/npm/eslint-plugin.md' },
         ],
       },
     ],
     sidebar: [
       {
-        title: "编码规范",
+        title: '编码规范',
         children: [
           {
-            title: "HTML 编码规范",
-            path: "/coding/html.md",
+            title: 'HTML 编码规范',
+            path: '/coding/html.md',
           },
           {
-            title: "CSS 编码规范",
-            path: "/coding/css.md",
+            title: 'CSS 编码规范',
+            path: '/coding/css.md',
           },
           {
-            title: "JavaScript 编码规范",
-            path: "/coding/javascript.md",
+            title: 'JavaScript 编码规范',
+            path: '/coding/javascript.md',
           },
           {
-            title: "Node 编码规范",
-            path: "/coding/node.md",
+            title: 'Node 编码规范',
+            path: '/coding/node.md',
           },
           {
-            title: "Typescript 编码规范",
-            path: "/coding/typescript.md",
+            title: 'Typescript 编码规范',
+            path: '/coding/typescript.md',
           },
         ],
       },
       {
-        title: "工程规范",
+        title: '工程规范',
         children: [
           {
-            title: "Git 规范",
-            path: "/engineering/git.md",
+            title: 'Git 规范',
+            path: '/engineering/git.md',
           },
           {
-            title: "文档规范",
-            path: "/engineering/doc.md",
+            title: '文档规范',
+            path: '/engineering/doc.md',
           },
           {
-            title: "CHANGELOG 规范",
-            path: "/engineering/changelog.md",
+            title: 'CHANGELOG 规范',
+            path: '/engineering/changelog.md',
           },
         ],
       },
+      {
+        title: 'NPM包',
+        children: [
+          { title: 'xy-commitlint-config', path: '/npm/commitlint.md' },
+          { title: 'xy-stylelint-config', path: '/npm/stylelint.md' },
+          { title: 'xy-markdownlint-config', path: '/npm/markdownlint.md' },
+          { title: 'xy-eslint-config', path: '/npm/eslint.md' },
+          { title: 'xy-eslint-plugin', path: '/npm/eslint-plugin.md' },
+        ],
+      },
     ],
-    logo: "/img/logo.png",
-    repo: "GXY1523/fe-spec",
+    logo: '/img/logo.png',
+    repo: 'GXY1523/fe-spec',
     searchMaxSuggestions: 10,
-    docsDir: "docs",
+    docsDir: 'docs',
     footer: {
       createYear: 2024,
     },
 
     extendFrontmatter: {
       author: {
-        name: "GXY",
-        link: "https://github.com/GXY1523/fe-spec",
+        name: 'GXY',
+        link: 'https://github.com/GXY1523/fe-spec',
       },
     },
   },
 
   head: [
-    ["link", { rel: "icon", href: "/img/logo.png" }],
+    ['link', { rel: 'icon', href: '/img/logo.png' }],
     [
-      "meta",
+      'meta',
       {
-        name: "keywords",
-        content: "前端编码规范工程化",
+        name: 'keywords',
+        content: '前端编码规范工程化',
       },
     ],
   ],
 
   plugins: <UserPlugins>[
     [
-      "one-click-copy",
+      'one-click-copy',
       {
-        copySelector: [
-          'div[class*="language-"] pre',
-          'div[class*="aside-code"] aside',
-        ],
-        copyMessage: "复制成功",
+        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'],
+        copyMessage: '复制成功',
         duration: 1000,
         showInMobile: false,
       },
     ],
 
     [
-      "vuepress-plugin-zooming",
+      'vuepress-plugin-zooming',
       {
-        selector: ".theme-vdoing-content img:not(.no-zoom)",
+        selector: '.theme-vdoing-content img:not(.no-zoom)',
         options: {
-          bgColor: "rgba(0,0,0,0.6)",
+          bgColor: 'rgba(0,0,0,0.6)',
         },
       },
     ],
   ],
-  extraWatchFiles: [".vuepress/config.ts"],
+  extraWatchFiles: ['.vuepress/config.ts'],
 });
---
+++ docs/npm/markdownlint.md
@@ -0,0 +1,36 @@
+---
+title: xy-markdownlint-config
+categories:
+  - 工程规范
+tags:
+  - 工程规范
+author:
+  name: GXY
+  link: https://github.com/GXY1523/fe-spec.git
+---
+
+# xy-markdownlint-config
+
+:::tip
+文档 规范
+:::
+
+支持配套的 [markdownlint 可共享配置](https://www.npmjs.com/package/markdownlint#optionsconfig)。
+
+## 安装
+
+需要先行安装 [markdownlint](https://www.npmjs.com/package/markdownlint)：
+
+```bash
+npm install xy-markdownlint-config markdownlint --save-dev
+```
+
+## 使用
+
+在 `.markdownlint.json` 中继承本包:
+
+```json
+{
+	"extends": "xy-markdownlint-config"
+}
+```
